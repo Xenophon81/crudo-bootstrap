736112a885c59dbd4d5ca1e5d490c85b7945af9d
Peter van den Heuvel <pvdheuvel81@gmail.com>
2020-05-04 15:08:35 +0200

    price tables
    - bordred line

--- 02-crudo-bootstrap/js/script.js
+++ 02-crudo-bootstrap/js/script.js
@@ -1,20 +1,21 @@
-// var header = document.querySelector('header');
-// var hoogte_header = header.offsetTop;
-// var logo = document.querySelector('img.logo');
+var header = document.querySelector('.navbar');
+var hoogte_header = header.offsetTop;
+var logo = document.querySelector('img.logo');
 
-// window.onscroll = function () {
-//     // https://developer.mozilla.org/en-US/docs/Web/API/Window/pageYOffset
-//     if (window.pageYOffset > hoogte_header) {
-//         header.classList.add("stick-to-top");
-//         logo.src = "./img/logo_black.svg";
-//     } else {
-//         header.classList.remove("stick-to-top");
-//         logo.src = "./img/logo_white.svg";
-//     }
-// };
+window.onscroll = function () {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/pageYOffset
+    if (window.pageYOffset > hoogte_header) {
+        header.classList.add("stick-to-top");
+        logo.src = "./img/logo_black.svg";
+        header.style.backgroundColor = 'white';
+    } else {
+        header.classList.remove("stick-to-top");
+        logo.src = "./img/logo_white.svg";
+    }
+};
 
-var images = document.querySelectorAll('.img-parallax');
-// documentation: https://simpleparallax.com/#example-scale
-new simpleParallax(image, {
-    scale: 1.2
-});
+// var images = document.querySelectorAll('.img-parallax');
+// // documentation: https://simpleparallax.com/#example-scale
+// new simpleParallax(image, {
+//     scale: 1.2
+// });
